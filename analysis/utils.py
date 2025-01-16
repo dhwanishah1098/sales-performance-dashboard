80f2ff9c069e6b77e6451f593a9e57a066c9e798
Dhwani Shah <dhwanishah1098@gmail.com>
2025-01-16 11:00:00 +1100

    Add active regions helper

--- analysis/utils.py
+++ analysis/utils.py
@@ -4,3 +4,6 @@ def revenue_per_customer(df):
 
 def orders_per_day(df):
     return df.groupby("order_date")["order_id"].nunique()
+
+def active_regions(df):
+    return sorted(df["region"].dropna().unique().tolist())
