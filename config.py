894281b25cd368fe0484657a36880e5c5282429a
Dhwani Shah <dhwanishah1098@gmail.com>
2025-03-10 11:30:00 +1100

    Add project config and directory structure

---
+++ config.py
@@ -0,0 +1,8 @@
+# Dashboard configuration
+DATA_PATH = "data/sales_data.csv"
+REPORT_OUTPUT = "reports/"
+FISCAL_YEAR_START = 4  # April
+
+REGIONS = ["North", "South", "East", "West"]
+TARGET_MARGIN = 0.35
+REVENUE_TARGET_MONTHLY = 500_000
